c804f90ff0c8ec00c40d13d9969f0d5a79b47fe0
Matthew Johnson <jhamat@amazon.com>
2023-06-12 17:43:00 -0700

    Set up project config and baseline TS page

---
+++ src/app.tsx
@@ -0,0 +1,44 @@
+// use components from https://react-bootstrap.netlify.app/docs/getting-started/introduction
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from './img/logo192.png'
+
+export default function App() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                // alt="React Bootstrap logo"
+            />{" "}
+            React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+// set up following https://dev.to/alekseiberezkin/setting-up-react-typescript-app-without-create-react-app-oph
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './app'
+import './index.css'
+
+const container = document.getElementById('app-root')!
+const root = createRoot(container)
+root.render(<App />)
+
+{
+/* The following line can be included in your src/index.js or App.js file */
+// Taken from https://react-bootstrap.netlify.app/docs/getting-started/introduction#css
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
