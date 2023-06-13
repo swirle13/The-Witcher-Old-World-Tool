55df3f096cd74dbe42395400f4c5776ce019f379
Matthew Johnson <jhamat@amazon.com>
2023-06-12 17:49:44 -0700

    Add link to how to deploy on Github Pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ const container = document.getElementById('app-root')!
 const root = createRoot(container)
 root.render(<App />)
 
+// to publish website to Github Pages, follow: https://www.c-sharpcorner.com/article/how-to-deploy-react-application-on-github-pages/
+
 {
 /* The following line can be included in your src/index.js or App.js file */
 // Taken from https://react-bootstrap.netlify.app/docs/getting-started/introduction#css
