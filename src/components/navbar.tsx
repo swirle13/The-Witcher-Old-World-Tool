7ace96980abc7a248ab54c69a96e067bc61fdd7b
Matthew Johnson <jhamat@amazon.com>
2023-06-20 17:16:10 -0700

    Update icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import {default as BSNavbar} from 'react-bootstrap/Navbar';
+import { default as BSNavbar } from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import logo from '../../public/logo192.png';
 
 export default function Navbar() {
     return (
         <BSNavbar collapseOnSelect id='Navbar' bg="dark" expand="xl" className="bg-body-primary" variant="dark">
             <Container>
                 <BSNavbar.Brand href="/" className=''>
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{" "}
                     The Witcher: Old World Helper
                 </BSNavbar.Brand>
                 <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
