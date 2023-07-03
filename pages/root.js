49a5620709088c6f2898a337b652507d2c7784e2
Matthew Johnson <jhamat@amazon.com>
2023-07-02 23:57:17 -0700

    Updates

--- pages/root.js
+++ pages/root.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
+const Footer_1 = __importDefault(require("../components/Footer"));
 const Navbar_1 = __importDefault(require("../components/Navbar"));
 const react_router_dom_1 = require("react-router-dom");
 function Root() {
-    return ((0, jsx_runtime_1.jsxs)("div", { className: 'BaseContainer mx-0 px-0', children: [(0, jsx_runtime_1.jsx)(Navbar_1.default, {}), (0, jsx_runtime_1.jsx)(react_router_dom_1.Outlet, {})] }));
+    return ((0, jsx_runtime_1.jsxs)("div", { className: 'BaseContainer mx-0 px-0', children: [(0, jsx_runtime_1.jsx)(Navbar_1.default, {}), (0, jsx_runtime_1.jsx)(react_router_dom_1.Outlet, {}), (0, jsx_runtime_1.jsx)(Footer_1.default, {})] }));
 }
 exports.default = Root;
