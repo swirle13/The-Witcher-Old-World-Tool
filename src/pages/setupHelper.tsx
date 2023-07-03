f02e89922a3a52c7b9cc6e285c87174e27a0a7fe
Matthew Johnson <jhamat@amazon.com>
2023-07-02 18:07:46 -0700

    Add dev warning for Wild Hunt expansion in SetupHelper

--- src/pages/setupHelper.tsx
+++ src/pages/setupHelper.tsx
@@ -74,6 +74,7 @@ export default function SetupHelper() {
                                                 />
                                             </Col>
                                         ))}
+                                        Under development!
                                     </Row>
                                 </Col>
                             </Row>
