498d07497094eca1c0692bcfa8574c2c1d432658
Matthew Johnson <jhamat@amazon.com>
2023-07-03 22:56:05 -0700

    Remove unused code in MonsterRoller.tsx

--- src/pages/MonsterRoller.tsx
+++ src/pages/MonsterRoller.tsx
@@ -1,19 +1,9 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import MonsterPicker from "../components/MonsterPicker";
 
 export default function MonsterRoller() {
     return (
         <Container id="MonsterRoller">
-            <Row className='d-flex justify-content-center'>
-                <Col xs={12} md={4} className='d-flex justify-content-center'>
-                    {/* <Image src={LostMountExpansion} rounded fluid/> */}
-                </Col>
-            </Row>
-            <Row className='d-flex justify-content-center'>
-                <Col xs={10} md={4} className='d-flex justify-content-center'>
-                    {/* <Image src={AdventurePackHeader} rounded fluid/> */}
-                </Col>
-            </Row>
             <MonsterPicker HeaderText="Roll A New Monster"/>
         </Container>
     )
