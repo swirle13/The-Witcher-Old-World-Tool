1fd835bb0ac84d8dbb16ae85cd89ed87a4db03ec
Matthew Johnson <swirle13@gmail.com>
2023-09-27 17:55:18 -0700

    Improve webpack; fix favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,62 +2,66 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { default as BSNavbar } from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../../public/logo192.png';
+import logo from '../../src/img/logo192.png';
 import useResize from '../util/useResize';
 import { myLangs } from "../i18n";
 import { ReactNode } from 'react';
+import './Navbar.css';
 
 export default function Navbar({ t, i18n }) {
     const size = useResize();
 
     return (
-        <BSNavbar collapseOnSelect id='Navbar' expand="md" className="bg-body-secondary">
-            <Container>
-                <BSNavbar.Brand href="#/">
+        <BSNavbar collapseOnSelect id='Navbar' expand="lg" className="bg-body-secondary">
+            <Container className="p-6">
+                <BSNavbar.Brand href="#/" className='ps-3'>
                     <img
                         src={logo}
                         width="30"
                         height="30"
                         className="d-inline-block align-top"
                     />{" "}
-                    {size.width < 401 ? t("Old World Helper") : t("The Witcher: Old World Helper")}
+                    {/* TODO: determine length of title in translated language and adjust accordingly */}
+                    {size.width < 401 ? t("navbar.titleShort") : t("navbar.titleLong")}
                 </BSNavbar.Brand>
                 <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
                 <BSNavbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#/communityLinks">{t('Community Links')}</Nav.Link>
-                        <NavDropdown title={t("Gameplay Tools")} id="basic-nav-dropdown">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#/communityLinks">{t('navbar.communityLinks')}</Nav.Link>
+                        <NavDropdown title={t("navbar.gameplayTools")} id="basic-nav-dropdown" className='text-wrap'>
                             <NavDropdown.Item href="#/setupHelper">
-                                {t('Setup Helper')}
+                                {t('navbar.setupHelper')}
                             </NavDropdown.Item>
                             <NavDropdown.Item href="#/locationTokens">
-                                {t('Location Tokens')}
+                                {t('navbar.locationTokens')}
                             </NavDropdown.Item>
                             <NavDropdown.Item href="#/monsterRoller">
-                                {t('Monster Roller')}
+                                {t('navbar.monsterRoller')}
                             </NavDropdown.Item>
                             <NavDropdown.Item href="#/lostMount">
-                                {t('Lost Mount Locations')}
+                                {t('navbar.lostMount')}
                             </NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#/">
-                                {t('Browse All Tools')}
+                                {t('navbar.browseAllTools')}
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title={t("Project Help")} id="github-nav-dropdown">
+                        <NavDropdown title={t("navbar.projectHelp")} id="github-nav-dropdown">
                             <NavDropdown.Item href="https://github.com/swirle13/The-Witcher-Old-World-Tool-Issues/issues">
-                                Github Issues
+                                {t('navbar.githubIssues')}
                             </NavDropdown.Item>
                             <NavDropdown.Item href="https://github.com/swirle13/The-Witcher-Old-World-Tool-Issues/discussions">
-                                Github Discussions
+                                {t('navbar.githubDiscussions')}
                             </NavDropdown.Item>
                             <NavDropdown.Item href="https://github.com/users/swirle13/projects/2/views/3">
-                                Project Timeline
+                                {t('navbar.projectTimeline')}
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title={t("__flag")} id="lang-nav-dropdown">
-                            {Object.entries(myLangs).map(([code, val]): ReactNode =>
-                                (<NavDropdown.Item onClick={() => i18n.changeLanguage(code)}>{val}</NavDropdown.Item>)
+                        <NavDropdown title={t("navbar.__flag")} id="lang-nav-dropdown" align={"end"}>
+                            {Object.entries(myLangs).map(([code, val], index): ReactNode =>
+                                <NavDropdown.Item key={index} onClick={() => i18n.changeLanguage(code)}>
+                                    {val}
+                                </NavDropdown.Item>
                             )}
                         </NavDropdown>
                     </Nav>
